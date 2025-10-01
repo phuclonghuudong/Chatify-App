388f8dc87d98767a6dc302d433c4c6ae21bf750c
phuc <phucpham.241099@gmail.com>
2025-10-01 18:08:56 +0700

    fix CLIENT

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -4,7 +4,7 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:3005/api"
-      : import.meta.env.SERVER,
+      : import.meta.env.VITE_API_URL,
   //  "https://chatify-app-b4om.onrender.com",
   // "https://server-chatify-app.vercel.app/api",
   withCredentials: true,
